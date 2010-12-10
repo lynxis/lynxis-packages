8990fe2468fa6681b792014afbf58cc47f4879ae
Alexander Couzens <lynxis@c-base.org>
2010-12-10 03:52:59 +0100

    using sda3 instead of sdb2

--- kexec_loader/files/kexec_dualboot.sh
+++ kexec_loader/files/kexec_dualboot.sh
@@ -7,8 +7,8 @@ else
     touch /tmp/kexec_loaded
 fi
 
-sleep 600 &&
-$(mount -o ro /dev/sda3 /mnt/
-kexec -l /mnt/boot/vmlinuz-dual --append "block2mtd.block2mtd=/dev/sda3,65536,rootfs root=/dev/mtdblock0 rootfstype=squashfs rootwait console=tty0 console=ttyS0,38400n8 noinitrd reboot=bios"
-umount /dev/sda3
+sleep 60 &&
+$(mount /dev/sda1 /mnt/
+kexec -l /mnt/boot/vmlinuz --append "block2mtd.block2mtd=/dev/sda3,65536,rootfs root=/dev/mtdblock0 rootfstype=squashfs rootwait console=tty0 console=hvc0,38400n8 noinitrd reboot=bios"
+umount /dev/sda1
 kexec -e)

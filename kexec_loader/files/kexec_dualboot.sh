f748c9c1c501529365d65050ecbfc32a0816618a
Alexander Couzens <lynxis@c-base.org>
2010-12-12 00:54:11 +0100

    mount boot-partition read-only

--- kexec_loader/files/kexec_dualboot.sh
+++ kexec_loader/files/kexec_dualboot.sh
@@ -8,7 +8,7 @@ else
 fi
 
 sleep 60 &&
-$(mount /dev/sda1 /mnt/
+$(mount -o ro /dev/sda1 /mnt/
 kexec -l /mnt/boot/vmlinuz-2nd --append="block2mtd.block2mtd=/dev/sda3,65536,rootfs root=/dev/mtdblock0 rootfstype=squashfs rootwait console=tty0 console=ttyS0,38400n8 noinitrd reboot=bios"
 umount /dev/sda1
 kexec -e)

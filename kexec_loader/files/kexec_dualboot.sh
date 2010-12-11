24af15a7679c6e0467328299b40457801e26c708
Alexander Couzens <lynxis@c-base.org>
2010-12-12 00:54:24 +0100

    sync before kexec new kernel

--- kexec_loader/files/kexec_dualboot.sh
+++ kexec_loader/files/kexec_dualboot.sh
@@ -11,4 +11,5 @@ sleep 60 &&
 $(mount -o ro /dev/sda1 /mnt/
 kexec -l /mnt/boot/vmlinuz-2nd --append="block2mtd.block2mtd=/dev/sda3,65536,rootfs root=/dev/mtdblock0 rootfstype=squashfs rootwait console=tty0 console=ttyS0,38400n8 noinitrd reboot=bios"
 umount /dev/sda1
+sync
 kexec -e)
